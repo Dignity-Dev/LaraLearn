579b545db3cff2f92c9bf738ff7cb4dadc5b315c
Basheer Haadi <bashirhaadiy@gmail.com>
2025-08-02 00:23:50 +0100

    for loop in php

---
+++ learnphp/for.php
@@ -0,0 +1,10 @@
+<?php
+echo "Rocket Lauch Countdown:\n";
+
+for ($i = 10; $i >0; $i--){
+    echo "$i\n";
+    if ($i == 1) {
+        echo "Liftoff!\n";
+    }
+    sleep(1); // Simulate a countdown delay
+}
