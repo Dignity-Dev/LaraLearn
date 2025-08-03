70a229b13100bbde17d386657aaf08dd542b3cdd
Basheer Haadi <bashirhaadiy@gmail.com>
2025-08-04 00:30:28 +0100

    Reference

---
+++ learnphp/refs.php
@@ -0,0 +1,14 @@
+<?php
+
+$person = "Basheer";
+
+$client ="Ridwan";
+
+var_dump($person, $client);
+
+$client = "Haadi";
+
+var_dump($person, $client);
+
+$person = "Haadi";
+var_dump($person, $client);
